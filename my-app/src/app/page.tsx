f9eef96f849b3a6dbb8fff7a8a47b366e6ef1ae8
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2025-01-02 08:04:02 -0600

    sets up directory for components with alias imports

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
+import Button from '@components/ui/Button';
 
 export default function page() {
   return (
-    <div>page</div>
+    <div><Button/></div>
   )
 }
