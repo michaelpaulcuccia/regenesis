1f88dfc460352c313d0d70a081f39f57971e67fa
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2025-01-10 17:17:46 -0600

    updates alignment

--- my-app/components/ui/NavBar.tsx
+++ my-app/components/ui/NavBar.tsx
@@ -34,6 +34,9 @@ export default function NavBar() {
           <div className="text-gray-700 mr-8 hover:text-black hover:font-bold cursor-pointer">
             Pricing
           </div>
+        </div>
+
+        <div className="flex flex-row items-center hidden md:flex">
           <Button text="Get Started" href="/signup" color="primary" />
         </div>
 
