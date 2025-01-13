b6edd7429258626cb8c7f2b994ff3daebbd110bc
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2025-01-13 09:06:24 -0600

    updates homepage

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,12 +1,23 @@
 "use client";
 import React from "react";
+import Link from "next/link";
 
 export default function page() {
   return (
-    <h2 className="font-medium tracking-tight text-slate-900 text-6xl text-center animate-blur-in">
-      Development <span className="text-[#2563EB]">made simple</span>
+    <>
+      <h2 className="font-medium tracking-tight text-slate-900 text-6xl text-center animate-blur-in">
+        Sign up <span className="text-[#2563EB]">made simple</span>
+        <br />
+        for your business needs
+      </h2>
       <br />
-      for your business needs
-    </h2>
+      <p className="font-normal tracking-tight text-slate-900 text-md text-center animate-blur-in">
+        Explore this applications enrollment process by clicking the{" "}
+        <Link href="/signup">
+          <span className="text-[#2563EB]">Get Started</span>
+        </Link>{" "}
+        button.
+      </p>
+    </>
   );
 }
